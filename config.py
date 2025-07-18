296c8e077c7d353e1d5f9a79ffeaf5b41106e9f9
wtl <1257942023@qq.com>
2025-07-18 15:39:52 +0800

    去除yolo模型

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ MODEL_VL3 = "qwen2.5-vl-3b-instruct"
 MODEL_VL72 = "qwen2.5-vl-72b-instruct"
 MODEL_QVQ = "qvq-72b-preview"
 MODEL_7 = "qwen2.5-7b-instruct-1m"
-MODEL_MAX_VL = "qwen-vl-max-2025-04-08"
+MODEL_MAX_VL = "qwen-vl-max"
 MODEL_MAX = "qwen-max-2025-01-25"
-BASE_URL = "https://dashscope.aliyuncs.com/compatible-mode/v1"
+BASE_URL = "https://wcode.net/api/gpt/v1"
 
 
--- nodes/agent_node.py
+++ nodes/agent_node.py
@@ -9,7 +9,8 @@ from config import *
 from utils import check_queue
 
 model = YOLO("models/yolo11x.pt")  # 使用 YOLOv11 Extra-Large 模型
-large_llm = LLM(init_msg="你是一个执行搜救任务的人工智能助手，请根据信息判断附近是否有被困人员，如果有，请从操作库中选择需要执行的操作(每次只可选择1种操作)。")
+small_llm = LLM(init_msg="你是一个执行搜救任务的人工智能助手，请根据信息判断附近是否有被困人员。")
+large_llm = LLM(model=MODEL_MAX_VL, init_msg="你是一个执行搜救任务的人工智能助手，请根据信息判断附近是否有被困人员，如果有，请从操作库中选择需要执行的操作(每次只可选择1种操作)。")
 
 def yolo_fliter(image: np.uint8) -> tuple[np.uint8, list]:
     """
@@ -54,7 +55,7 @@ async def observe(img_queue: asyncio.Queue, action_queue: asyncio.Queue, feedbac
     """
     while True:
         image = await img_queue.get()
-        answer = large_llm.call("参考YOLO模型的识别结果判断附近是否可能有被困人员，请回答'有'或'没有'。", image)
+        answer = small_llm.call("参考YOLO模型的识别结果判断附近是否可能有被困人员，请回答'有'或'没有'。", image)
         if "没有" in answer:
             break
         elif "有" in answer:
@@ -63,8 +64,8 @@ async def observe(img_queue: asyncio.Queue, action_queue: asyncio.Queue, feedbac
             await make_decision(img_queue, action_queue, feedback_queue)
             break
         else:
-            answer = large_llm.call("格式输出错误，请回答'有'或'没有'。", image)
-        large_llm.clear_messages()
+            answer = small_llm.call("格式输出错误，请回答'有'或'没有'。", image)
+        small_llm.clear_messages()
         await asyncio.sleep(0.1)
 
 
@@ -77,32 +78,40 @@ async def make_decision(img_queue: asyncio.Queue, action_queue: asyncio.Queue, f
         feedback_queue:反馈信息队列
     """
     image = await img_queue.get()
+    answer = large_llm.call("附近是否可能有被困人员，请回答'有'或'没有'。", image)
     
     while True:
-        answer = large_llm.call("请选择需要执行的操作。(可执行操作包含:'移动至被困人员处','在被困人员物品周围搜寻被困人员','通知总部找到被困人员','向被困人员发放紧急救援物资','安抚被困人员','继续寻找其他被困人员')", image)
-        while True:
-            if "在被困人员物品周围搜寻被困人员" in answer:
-                action_queue.put_nowait("seek")
-            elif "移动至被困人员处" in answer:
-                action_queue.put_nowait("moveto")
-            elif "通知总部找到被困人员" in answer:
-                action_queue.put_nowait("broadcast")
-            elif "向被困人员发放紧急救援物资" in answer:
-                action_queue.put_nowait("drop")
-            elif "安抚被困人员" in answer:
-                action_queue.put_nowait("console")
-            elif "继续寻找其他被困人员" in answer:
-                action_queue.put_nowait("seek_next")
+        if "没有" in answer:
+            large_llm.del_last_message()
+            break
+        elif "有" in answer:
+            answer = large_llm.call("请选择需要执行的操作。(可执行操作包含:'移动至被困人员处','在被困人员物品周围搜寻被困人员','通知总部找到被困人员','向被困人员发放紧急救援物资','安抚被困人员','继续寻找其他被困人员')", image)
+            while True:
+                if "在被困人员物品周围搜寻被困人员" in answer:
+                    action_queue.put_nowait("seek")
+                elif "移动至被困人员处" in answer:
+                    action_queue.put_nowait("moveto")
+                elif "通知总部找到被困人员" in answer:
+                    action_queue.put_nowait("broadcast")
+                elif "向被困人员发放紧急救援物资" in answer:
+                    action_queue.put_nowait("drop")
+                elif "安抚被困人员" in answer:
+                    action_queue.put_nowait("console")
+                elif "继续寻找其他被困人员" in answer:
+                    action_queue.put_nowait("seek_next")
+                    feedback = await check_feedback(feedback_queue)
+                    large_llm.user_put(feedback)
+                    break
+                else:
+                    answer = large_llm.call("未从回答中识别到可执行操作，请输出正确的需要执行的操作。(可执行操作包含:'移动至被困人员处','在被困人员物品周围搜寻被困人员','通知总部找到被困人员','向被困人员发放紧急救援物资','安抚被困人员','继续寻找其他被困人员')", image)
+                
                 feedback = await check_feedback(feedback_queue)
-                large_llm.user_put(feedback)
-                break
-            else:
-                answer = large_llm.call("未从回答中识别到可执行操作，请输出正确的需要执行的操作。(可执行操作包含:'移动至被困人员处','在被困人员物品周围搜寻被困人员','通知总部找到被困人员','向被困人员发放紧急救援物资','安抚被困人员','继续寻找其他被困人员')", image)
-            
-            feedback = await check_feedback(feedback_queue)
-            image = await img_queue.get()
-            answer = large_llm.call(feedback, image)
-        break
+                image = await img_queue.get()
+                answer = large_llm.call(feedback, image)
+            break
+        else:
+            answer = large_llm.call("格式输出错误，请回答选择需要执行的操作或'没有'。", image)
+
 
 
 async def check_feedback(feedback_queue: asyncio.Queue):
